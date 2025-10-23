a1bcad1d5dd4fc75cebdca4a10607d5ce120448d
shansha89 <karthicktech89@gmail.com>
2025-10-23 23:43:56 +0530

    feature: Create Entity

---
+++ src/main/java/com/amce/edusite/student_management/entity/Student.java
@@ -0,0 +1,34 @@
+package com.amce.edusite.student_management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message =  "Email is mandatory")
+    private String email;
+
+    @NotBlank(message = "Course is mandatory")
+    private String course;
+}
